e736f3ca4167c00a1dd448a095e772d088ea850d
Samson Ajifowoke <samsonajifowoke@gmail.com>
2026-08-26 19:30:40 +0100

    solve a sorting question on leetcode

--- sorting.py
+++ sorting.py
@@ -76,3 +76,54 @@ class Solution:
 
 # Time complexity - O(n^2)
 # Space complexity - O(n^2)
+
+# Leetcode - Problem: 1630
+
+"""A sequence of numbers is called arithmetic if it consists of at least two elements, and the difference between every two consecutive elements is the same.
+More formally, a sequence s is arithmetic if and only if s[i+1] - s[i] == s[1] - s[0] for all valid i.
+
+For example, these are arithmetic sequences:
+
+1, 3, 5, 7, 9
+7, 7, 7, 7
+3, -1, -5, -9
+The following sequence is not arithmetic:
+
+1, 1, 2, 5, 7
+You are given an array of n integers, nums, and two arrays of m integers each, l and r, representing the m range queries, where the ith query is the range [l[i], r[i]]. All the arrays are 0-indexed.
+
+Return a list of boolean elements answer, where answer[i] is true if the subarray nums[l[i]], nums[l[i]+1], ... , nums[r[i]] can be rearranged to form an arithmetic sequence, and false otherwise.
+"""
+
+# Solution
+
+# Algorithm
+# 1. Run a loop from i=0 to m-1
+# 2. Get the subarray conrresponding to nums[l[i]:r[i]]
+# 3. Sort the subarry
+# 4. Run another loop from d=2 to length of subarray
+# 5. Check if consecutive integers have the same difference
+
+def checkArithmeticSubarrays(nums, l, r):
+    """
+    :type nums: List[int]
+    :type l: List[int]
+    :type r: List[int]
+    :rtype: List[bool]
+    """
+
+    answer = []
+    for i in range(len(l)):
+        sub_array = nums[l[i]:r[i]+1]
+        sub_array.sort()
+
+        is_arithmetic = True
+        for d in range(2, len(sub_array)):
+            is_con = sub_array[d] - sub_array[d-1] == sub_array[d-1] - sub_array[d-2]
+            is_arithmetic = is_arithmetic and is_con
+
+        answer.append(is_arithmetic)
+    return answer
+
+# time complexity -> O(m*n(logn))
+# space complexity -> O(n*m)
