55d77b80d1fc050cd4e6e48529f25f48bb0e4b30
Samson Ajifowoke <samsonajifowoke@gmail.com>
2026-09-09 21:39:15 +0100

    solve array questions

--- arrays.py
+++ arrays.py
@@ -387,4 +387,65 @@ def plusOne(digits):
         digits.insert(0, carry)
     return digits
 
-print(plusOne([9]))
+
+# Leetcode - Problem: 13
+
+def threeSum(nums):
+    nums.sort()
+    res, processed = [], set()
+
+    for i in range(len(nums)-1):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        j, k = i+1, len(nums)-1
+        while j < k:
+            sum = nums[i] + nums[j] + nums[k]
+
+            if sum == 0:
+                if (nums[i], nums[j], nums[k]) not in processed:
+                    res.append([nums[i], nums[j], nums[k]])
+                    processed.add((nums[i], nums[j], nums[k]))
+
+                j += 1
+                k -= 1
+            elif sum > 0:
+                k -= 1
+                while k < j and nums[k] == nums[k+1]:
+                    k -= 1
+            else:
+                j += 1
+                while k < j and nums[j] == nums[j-1]:
+                    j += 1
+    return res
+
+
+# Leetcode - Problem: 16
+
+def threeSumClosest(nums, target):
+    res = float("+inf")
+    nums.sort()
+
+    for i in range(len(nums)-1):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        j, k = i+1, len(nums)-1
+        while j < k:
+            sum = nums[i] + nums[j] + nums[k]
+
+            if sum == target:
+                return sum
+
+            if abs(target-sum) < abs(target-res):
+                res = sum
+
+            if sum <= target:
+                j += 1
+                while k < j and nums[j] == nums[j-1]:
+                    j += 1
+            else:
+                k -= 1
+                while k < j and nums[j] == nums[j-1]:
+                    k -= 1
+    return res
